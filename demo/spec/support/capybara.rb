7d1ae2beb860895f585e47fc17b74ca39a89c252
Mountain Star <github@mtnstar.net>
2024-12-25 19:11:38 +1300

    introduce and use CI env variable for feature specs

--- demo/spec/support/capybara.rb
+++ demo/spec/support/capybara.rb
@@ -1,12 +1,14 @@
-require 'capybara/rails'
-require 'selenium/webdriver'
+require "capybara/rails"
+require "selenium/webdriver"
 
 Capybara.register_driver :firefox do |app|
   options = Selenium::WebDriver::Firefox::Options.new
   
-  options.add_argument('--headless') unless ENV['HEADLESS'] == 'false'
-  options.add_argument('--disable-gpu') # Recommended for headless mode
-  options.add_argument('--no-sandbox') # Recommended for CI environments
+  unless ENV['HEADLESS'] == "false"
+    options.add_argument("--headless") unless ENV["HEADLESS"] == "false"
+    options.add_argument("--disable-gpu")
+  end
+  options.add_argument('--no-sandbox') if ENV["CI"] == "true"
 
   Capybara::Selenium::Driver.new(
     app,
