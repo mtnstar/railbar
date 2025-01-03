e6b0ae491f64c9a38226c55e7fcd0680b164b73f
Mountain Star <github@mtnstar.net>
2025-01-03 20:01:20 +1300

    Wip: implement turbo stream status

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace "railbar" do
     get "turbo_frame_status", to: "turbo_frame_status#show"
+    post "turbo_stream_status", to: "turbo_stream_status#update"
   end
 end
