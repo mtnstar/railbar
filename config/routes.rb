041d03cd2da9ad49cf0490fbe0e44c3c147b1e79
Mountain Star <github@mtnstar.net>
2025-01-03 16:55:17 +1300

    Define main app routes in gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
-Railbar::Engine.routes.draw do
-  get "turbo_frame_status", to: "turbo_frame_status#show"
+Rails.application.routes.draw do
+  namespace "railbar" do
+    get "turbo_frame_status", to: "turbo_frame_status#show"
+  end
 end
--- lib/railbar/engine.rb
+++ lib/railbar/engine.rb
@@ -7,14 +7,5 @@ module Railbar
         helper Railbar::ApplicationHelper
       end
     end
-
-    #initializer "railbar.view_paths" do |app|
-      #ActionController::Base.prepend_view_path(File.expand_path("app/views", __dir__))
-    #end
-
-    #initializer "railbar.load_custom_routes" do
-      #puts "Custom routes for Railbar loaded from app/config/routes.rb"
-      #paths["config/routes"] = "app/config/routes.rb"
-    #end
   end
 end
