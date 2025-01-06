b34e6f904cb693fdaba45f136563aa5d8c642f82
Mountain Star <github@mtnstar.net>
2025-01-06 18:25:04 +1300

    Revert "Only define helper in rails for non api controllers"
    This reverts commit 85875afe0bfd24ebe88d4bed4082111ef8f102c2.

--- lib/railbar/engine.rb
+++ lib/railbar/engine.rb
@@ -3,8 +3,7 @@ module Railbar
     isolate_namespace Railbar
 
     initializer "railbar.helpers" do
-      # Include helper only for non-API controllers
-      if self < ActionController::Base
+      ActiveSupport.on_load(:action_controller) do
         helper Railbar::ApplicationHelper
       end
     end
